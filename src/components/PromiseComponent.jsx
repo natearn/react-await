82b62f67c1e80119eb17fd34a99296a933096254
Nathan Arnold <natearn@natearn.info>
2019-02-12 18:01:12 -0500

    Add PromiseComponent

---
+++ src/components/PromiseComponent.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export class PromiseComponent extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { resolved: null };
+	}
+
+	componentDidMount() {
+		const promise = this.props.children;
+		promise.then(resolved => this.setState({ resolved }));
+	}
+
+	render() {
+		const Pending = this.props.pending;
+		return this.state.resolved || <Pending />;
+	}
+}
