48c57d07bf29299401f8309790494a8b01046acb
Nathan Arnold <natearn@natearn.info>
2019-02-12 19:02:20 -0500

    Remove unused webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,6 @@ const path = require('path');
 
 module.exports = {
     "mode": "development",
-    //"entry": "src/index.js",
-    "output": {
-        "path": path.resolve(__dirname, '/dist'),
-        "filename": "[name].[chunkhash:8].js"
-    },
     "devtool": "source-map",
     "module": {
         "rules": [
