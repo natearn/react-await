a2e455d38004020dcfaa50880d12f26faf331aad
Nathan Arnold <natearn@natearn.info>
2019-07-11 17:43:24 -0400

    Add linting and prettier

--- src/components/Await/Await.jsx
+++ src/components/Await/Await.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-const Await = ({pending, children}) => {
-	const [result,resolve] = useState(null);
+const Await = ({ pending, children }) => {
+  const [result, resolve] = useState(null);
 
-	useEffect(() => {
-		children.then(resolve)
-	}, [children]);
+  useEffect(() => {
+    children.then(resolve);
+  }, [children]);
 
-	return result || pending();
+  return result || pending();
 };
 
 export default Await;
